e61063f67597b693e35ada83068caaeaabb3cb10
VojtechBobcik <xbobcik@mendelu.cz>
2024-02-23 12:26:46 +0100

    init of project, first components made

---
+++ src/components/Book.js
@@ -0,0 +1,12 @@
+import "./Book.css"
+import OrderButton from "./OrderButton"
+
+const Book = () => {
+    return <>
+        <h2 className= "main-heading">Nazev knihy</h2>
+        <OrderButton />
+    </>
+    
+}
+
+export default Book
---
+++ src/components/OrderButton.js
@@ -0,0 +1,11 @@
+import "./OrderButton.css"
+
+const OrderButton = () =>{
+    return <>
+        <button className="btn" type="button">
+            Objednat
+        </button>
+    </>
+}
+
+export default OrderButton
